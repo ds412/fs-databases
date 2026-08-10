51bf2a35665b21a887aefcc21f913945a7fb731b
Dennis Seeldrayers <dennis.seeldrayers@gmail.com>
2026-08-10 16:51:26 -0400

    Fullstackopen - Relational DBs: completed chapter 3, exercises 5-7, restructuring application and adding middleware

--- index.js
+++ index.js
@@ -1,76 +1,30 @@
-require('dotenv').config()
 const express = require('express')
-const { Sequelize, Model, DataTypes } = require('sequelize')
-
-// Express app
 const app = express()
+
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+
+const blogsRouter = require('./controllers/blogs')
+
 app.use(express.json())
 
-// Sequelize connection
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres'
-})
+app.use('/api/blogs', blogsRouter)
 
-// Blog data model
-class Blog extends Model {}
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
-})
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'SequelizeValidationError') {
+        return res.status(400).json({ error: error.message })
+    }
+    next(error)
+}
 
-// Ensure table exists
-Blog.sync()
+app.use(errorHandler)
 
-// Express routes
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll()
-  res.json(blogs)
-})
+const start = async () => {
+    await connectToDatabase()
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+}
 
-app.post('/api/blogs', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    res.json(blog)
-  } catch (error) {
-    res.status(400).json({ error })
-  }
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    await blog.destroy()
-    res.status(204).end()
-  } else {
-    res.status(404).end()
-  }
-})
-
-// Start server
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+start()
